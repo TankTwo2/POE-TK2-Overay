d00f725dd57cd623a57ee4509ea7338ba2f5ffc3
Tanktwo2 <rhkdtjr9@naver.com>
2022-10-03 09:06:01 +0900

    master : setting: 기본폴더 구조 세팅
    <description>
    <issue-number>

--- src/main/main.ts
+++ src/main/main.ts
@@ -75,6 +75,7 @@ const createWindow = async () => {
     height: 728,
     icon: getAssetPath('icon.png'),
     resizable: false,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
